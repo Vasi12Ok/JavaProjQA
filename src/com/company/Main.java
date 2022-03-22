47f1722fefd538b47c379a8e923df371daa9830c
Sipos <vasi.sipos78@gmail.com>
2022-03-22 22:48:49 +0100

    Tema Laborator 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println(" Hello World ");
+
+        System.out.println("  primul exercitiu 6.333 ");
+        System.out.println(" al doilea exercitiu 5.76");
+        System.out.println(" al treilea exercitiu ");
+
+
+
+
+
+
 
 
     }
