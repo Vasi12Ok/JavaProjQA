52663ce40fc07af0f0e6c641de824339732d446d
Sipos <vasi.sipos78@gmail.com>
2022-03-23 00:39:28 +0100

    Tema Laborator 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(" Hello World ");
+        System.out.println(" Hello ");
+        System.out.println(" -5 + 8 * 6" );
 
-        System.out.println("  primul exercitiu 6.333 ");
-        System.out.println(" al doilea exercitiu 5.76");
-        System.out.println(" al treilea exercitiu ");
+        int x = 5;
+        int y = 8;
+        int z = 6;
+
+        int suma = x + y * z;
+        System.out.println("suma totala este: 18" );
+
+        System.out.println("(55+9) % 9");
+
+        int a = 55;
+        int b = 9;
+        int c = 9;
+
+        System.out.println("suma totala este: 1"  );
+
+
+        System.out.println("20 + -3*5 / 8 ");
+
+    }    float a = 20;
+            float b = -3*5;
+            float c = 8;
 
 
 
 
-
-
-
-
-    }
 
 
 
