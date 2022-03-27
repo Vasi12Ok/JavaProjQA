fc2a2818fd1206a5ab24f896b07aeff185629954
Sipos <vasi.sipos78@gmail.com>
2022-03-27 13:29:03 +0200

    Tema Laborator 1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+       public static void main(String[] args) {
+
+          double result = sum(2, 3);
+          System.out.println(reuslt * 2);
+          System.out.println("Rezultatul este:" + sum(3, 5));
+
+        }
+
+
+
+
+
+        public static void sum(int firstNumber, double secondNumber) {
+            double x = firstNumber / secondNumber;
+            return x;
+
+
+        }
+
+
+
+
